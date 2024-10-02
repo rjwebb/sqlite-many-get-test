f0ea79cb3c2bcb8d39ef5fc6c100a3ba95c589bb
Bob Webb <bob.wbb@gmail.com>
2024-10-02 14:27:44 +0200

    Add script for running all the tests in series

---
+++ run_all.sh
@@ -0,0 +1,17 @@
+node benchmark.js 10 separate
+node benchmark.js 20 separate
+node benchmark.js 50 separate
+node benchmark.js 100 separate
+node benchmark.js 1000 separate
+
+node benchmark.js 10 combined
+node benchmark.js 20 combined
+node benchmark.js 50 combined
+node benchmark.js 100 combined
+node benchmark.js 1000 combined
+
+node benchmark.js 10 or
+node benchmark.js 20 or
+node benchmark.js 50 or
+node benchmark.js 100 or
+node benchmark.js 1000 or
